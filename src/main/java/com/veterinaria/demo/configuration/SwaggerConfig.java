06300a2e1f1aeae5cef56112f1fe28940aeb1b56
Gabriella Franco <gabriella-nena@hotmail.com>
2025-05-26 15:17:45 -0300

    Definidas configurações .env, Swagger e BD rodando em contêiner Docker

---
+++ src/main/java/com/veterinaria/demo/configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.veterinaria.demo.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistema Veterinário API")
+                        .version("1.0")
+                        .description("API para gerenciamento de clínica veterinária")
+                );
+    }
+}
