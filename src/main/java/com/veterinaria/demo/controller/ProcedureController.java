6e68fd3e9f1e56777cbc82ca16411e5b2c69fa38
Gabriella Franco <gabriella-nena@hotmail.com>
2025-05-28 16:13:54 -0300

    Controllers das entidades User e Procedure criados

---
+++ src/main/java/com/veterinaria/demo/controller/ProcedureController.java
@@ -0,0 +1,63 @@
+package com.veterinaria.demo.controller;
+
+import com.veterinaria.demo.model.dto.procedure.CreateProcedureDTO;
+import com.veterinaria.demo.model.dto.procedure.GetProcedureDTO;
+import com.veterinaria.demo.service.ProcedureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/procedures")
+public class ProcedureController {
+
+    private final ProcedureService procedureService;
+
+    @Operation(
+            summary = "Returns a list with all existing procedures",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Operation successful"),
+                    @ApiResponse(responseCode = "204", description = "No content to show")
+            }
+    )
+    @GetMapping
+    public ResponseEntity<List<GetProcedureDTO>> getAllProcedures() {
+        var procedures = procedureService.getAllProcedures();
+        return procedures.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(procedures);
+    }
+
+    @Operation(
+            summary = "Finds a procedure that matches the id provided",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Operation successful"),
+                    @ApiResponse(responseCode = "404", description = "Not found")
+            }
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<GetProcedureDTO> getProcedureById(@PathVariable Long id) {
+        return ResponseEntity.ok(procedureService.getProcedureById(id));
+    }
+
+    @Operation(
+            summary = "Creates a new procedure",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Operation successful"),
+                    @ApiResponse(responseCode = "400", description = "Invalid data")
+            }
+    )
+    @PostMapping
+    public ResponseEntity<GetProcedureDTO> createProcedure(@Valid @RequestBody CreateProcedureDTO procedureDTO) {
+        var procedure = procedureService.createProcedure(procedureDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(procedure.id()).toUri();
+        return ResponseEntity.created(uri).body(procedure);
+    }
+}
